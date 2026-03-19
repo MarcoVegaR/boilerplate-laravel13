83ac03d8f48accba30d4e7502c99092ecb2f3fc5
marcovegar <marcovega11@gmail.com>
2026-03-19 08:19:33 -0400

    test: align example home expectation

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('returns a successful response', function () {
+test('guests are redirected from root to login', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response->assertRedirect(route('login', absolute: false));
 });
